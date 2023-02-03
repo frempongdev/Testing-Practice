df37dcf3ddbec215644f10d31fe888f3f5908894
Samuel Amankwaa-Frempong <117144028+frempongdev@users.noreply.github.com>
2023-02-03 07:07:03 +0000

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,17 +1,3 @@
-// const stringLength = require('./index.js');
-
-// test('stringLength of Mango', () => {
-//   expect(stringLength('Mango')).toBe(5);
-// });
-
-// test('stringLength of Microverse', () => {
-//   expect(stringLength('Microverse')).toBe(10);
-// });
-
-// test('stringLength of Frempong', () => {
-//   expect(stringLength('Frempong')).toBe(8);
-// });
-
 const { stringLength, reverseString } = require('./index.js');
 
 describe('stringLength', () => {
